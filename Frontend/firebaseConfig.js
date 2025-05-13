271f18984b7e977b2f9be7f584733f4f514840b4
SyedMuqeem03 <Smuqeem03@gmail.com>
2025-05-13 23:28:48 +0530

    environment variables updated

--- Frontend/firebaseConfig.js
+++ Frontend/firebaseConfig.js
@@ -2,15 +2,23 @@ import { initializeApp } from 'firebase/app';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBR0ilpdGu4mbUvN4OI5zFwu9eJZrD0Oto",
-  authDomain: "nagish-app-a4d39.firebaseapp.com",
-  projectId: "nagish-app-a4d39",
-  storageBucket: "nagish-app-a4d39.firebasestorage.app",
-  messagingSenderId: "980142842351",
-  appId: "1:980142842351:web:621595121b462ae2b2161a"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
 // Initialize Firebase
